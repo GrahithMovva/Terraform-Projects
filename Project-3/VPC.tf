27a7168c8fd4346cdeecab811176c2533e1b53df
GrahithMovva <gm4960@rit.edu>
2024-06-25 12:31:57 +0530

    finished project 3

--- Project-3/VPC.tf
+++ Project-3/VPC.tf
@@ -3,22 +3,22 @@ resource "aws_vpc" "Main_vpc" {
 }
 
 resource "aws_subnet" "subnet-1" {
-    vpc_id = aws_vpc.Main_vpc.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "ap-south-1a"
-    map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.Main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet-2" {
-    vpc_id = aws_vpc.Main_vpc.id
-    cidr_block = "10.0.2.0/24"
-    availability_zone = "ap-south-1b"
-    map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.Main_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.Main_vpc.id
-  
+  vpc_id = aws_vpc.Main_vpc.id
+
 }
 
 resource "aws_route_table" "main-rt-table" {
@@ -31,11 +31,11 @@ resource "aws_route_table" "main-rt-table" {
 }
 
 resource "aws_route_table_association" "a" {
-    route_table_id = aws_route_table.main-rt-table.id
-    subnet_id = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.main-rt-table.id
+  subnet_id      = aws_subnet.subnet-1.id
 }
 
 resource "aws_route_table_association" "b" {
-    route_table_id = aws_route_table.main-rt-table.id
-    subnet_id = aws_subnet.subnet-2.id
+  route_table_id = aws_route_table.main-rt-table.id
+  subnet_id      = aws_subnet.subnet-2.id
 }
