0f64ebe63ff7c08b0a8645b2f841e476d5cfbe40
GrahithMovva <gm4960@rit.edu>
2024-06-27 10:36:41 +0530

    finished the first implementation (Except security groups)

--- Project-4/lambdas.tf
+++ Project-4/lambdas.tf
@@ -16,63 +16,11 @@ resource "aws_lambda_function" "secretManager-RDS-lamdba" {
   }
   environment {
     variables = {
-      SECRET_NAME = "rds-credentials"
+      SECRET_NAME = "rds-password"
       REGION_NAME = "ap-south-1"
-      DB_HOST = aws_db_instance.mysql.address
+      DB_HOST     = aws_db_instance.mysql.address
     }
   }
-
-}
-
-resource "aws_lambda_permission" "allow_secretManager" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.secretManager-lamdba.function_name
-  principal     = "secretsmanager.amazonaws.com"
-}
-
-resource "aws_lambda_function" "secretManager-lamdba" {
-  filename      = "lambda_function_payload.zip"
-  function_name = "lambda_SM_handler"
-  handler       = "lambda_SM_handler"
-  runtime       = "python3.12"
-  role          = aws_iam_role.iam_for_lambda.arn
-
-
-  vpc_config {
-    subnet_ids         = []
-    security_group_ids = []
-  }
-  environment {
-    variables = {
-      SECRET_NAME = "rds-credentials"
-    }
-  }
-
-}
-
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com", "secretsmanager.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-
-
-
-
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-
 
 }
 
@@ -97,49 +45,146 @@ resource "aws_iam_role" "lambda-1-role" {
 EOF
 }
 
-# resource "aws_iam_role" "lambda-2-role" {
-#   name        = "example_lambda2_exec_role"
-#   description = "Execution role for the lambda function"
+resource "aws_iam_role_policy_attachment" "policy-1-attach" {
+  role       = aws_iam_role.lambda-1-role.name
+  policy_arn = aws_iam_policy.policy-rotation-2.arn
 
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Id": "default",
-#   "Statement": [
-#     {
-#       "Sid": "SecretsManagerRDSMySQLRotationSingleUser3fb-SecretRotationScheduleHost-LambdaPermission-FRv0GuDPEta7",
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": "secretsmanager.amazonaws.com"
-#       },
-#       "Action": "lambda:InvokeFunction",
-#       "Resource": "arn:aws:lambda:ap-south-1:654797133855:function:lambda_SM_handler",
-#       "Condition": {
-#         "StringEquals": {
-#           "AWS:SourceAccount": "654797133855"
-#         }
-#       }
-#     }
-#   ]
-# }
-# EOF
+}
 
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "secretsmanager.amazonaws.com"
-#       },
-#       "Effect": "Allow",
-#       "Sid": ""
-#     }
-#   ]
-# }
-# EOF
-# }
+resource "aws_iam_role_policy_attachment" "policy-2-attach" {
+  role       = aws_iam_role.lambda-1-role.name
+  policy_arn = aws_iam_policy.policy-RDS-Process.arn
+
+}
+
+
+
+resource "aws_lambda_function" "RDS-Data-procesing-lamdba" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_RDS_handler"
+  handler       = "lambda_RDS_handler"
+  runtime       = "python3.12"
+  role          = aws_iam_role.lambda-RDS-Analyze-role.arn
+  vpc_config {
+    subnet_ids         = []
+    security_group_ids = []
+  }
+  environment {
+    variables = {
+      SECRET_NAME = "rds-password"
+      REGION_NAME = "ap-south-1"
+      DB_HOST     = aws_db_instance.mysql.address
+    }
+  }
+
+}
+
+resource "aws_iam_role" "lambda-RDS-Analyze-role" {
+  name        = "data-analyze_lambda_exec_role"
+  description = "Execution role for the lambda function"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_permission" "hour-event-permission" {
+  statement_id  = "AllowExecutonFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.RDS-Data-procesing-lamdba.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.Hour.arn
+}
+
+resource "aws_lambda_permission" "minute-event-permission" {
+  statement_id  = "AllowExecutonFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.RDS-Data-procesing-lamdba.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.minute.arn
+}
+
+resource "aws_iam_policy" "policy-RDS-Process" {
+  name        = "lambda-rds-process-access-policy"
+  description = "Policy for Lambda to access RDS and analyze data"
+
+  policy = <<EOF
+{
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Sid": "VisualEditor0",
+			"Effect": "Allow",
+			"Action": "rds:ModifyDBInstance",
+			"Resource": "*"
+		}
+	]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "policy-data-analyze-role-attach" {
+  role       = aws_iam_role.lambda-RDS-Analyze-role.name
+  policy_arn = aws_iam_policy.policy-RDS-Process.arn
+}
+
+resource "aws_lambda_function" "secretManager-lamdba" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_SM_handler"
+  handler       = "lambda_SM_handler"
+  runtime       = "python3.12"
+  role          = aws_iam_role.iam_for_lambda.arn
+
+
+  vpc_config {
+    subnet_ids         = []
+    security_group_ids = []
+  }
+  environment {
+    variables = {
+      SECRET_NAME = "rds-password"
+    }
+  }
+
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com", "secretsmanager.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_lambda_permission" "allow_secretManager" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.secretManager-lamdba.function_name
+  principal     = "secretsmanager.amazonaws.com"
+}
 
 resource "aws_iam_policy" "policy-rotation-1" {
   name        = "rotation_1_lambda_policy"
@@ -195,6 +240,77 @@ resource "aws_iam_policy" "policy-rotation-2" {
 EOF
 }
 
+resource "aws_iam_role_policy_attachment" "lambda-2-rotation-policy-1-attach" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.policy-rotation-1.arn
+
+}
+
+resource "aws_iam_role_policy_attachment" "lambda-2-rotation-policy-arn-1-attach" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+
+}
+
+resource "aws_iam_role_policy_attachment" "lambda-2-rotation-policy-arn-2-attach" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+
+}
+
+resource "aws_iam_role_policy_attachment" "lambda-2-rotation-policy-2-attach" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.policy-rotation-2.arn
+
+}
+
+
+# resource "aws_iam_role" "lambda-2-role" {
+#   name        = "example_lambda2_exec_role"
+#   description = "Execution role for the lambda function"
+
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Id": "default",
+#   "Statement": [
+#     {
+#       "Sid": "SecretsManagerRDSMySQLRotationSingleUser3fb-SecretRotationScheduleHost-LambdaPermission-FRv0GuDPEta7",
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "secretsmanager.amazonaws.com"
+#       },
+#       "Action": "lambda:InvokeFunction",
+#       "Resource": "arn:aws:lambda:ap-south-1:654797133855:function:lambda_SM_handler",
+#       "Condition": {
+#         "StringEquals": {
+#           "AWS:SourceAccount": "654797133855"
+#         }
+#       }
+#     }
+#   ]
+# }
+# EOF
+
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": "sts:AssumeRole",
+#       "Principal": {
+#         "Service": "secretsmanager.amazonaws.com"
+#       },
+#       "Effect": "Allow",
+#       "Sid": ""
+#     }
+#   ]
+# }
+# EOF
+# }
+
+
+
 # resource "aws_iam_policy" "policy-rotation-3" {
 #   name        = "rotaion_3_lambda_policy"
 #   description = "secrets manager policy for the lambda function"
@@ -222,184 +338,10 @@ EOF
 # EOF
 # }
 
-resource "aws_iam_policy" "policy-2" {
-  name        = "lambda-rds-write-access-policy"
-  description = "Policy for Lambda to access RDS"
 
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "VisualEditor0",
-            "Effect": "Allow",
-            "Action": [
-                "rds:DeleteBlueGreenDeployment",
-                "rds:StartDBCluster",
-                "rds:DeleteGlobalCluster",
-                "rds:RestoreDBInstanceFromS3",
-                "rds:ResetDBParameterGroup",
-                "rds:CrossRegionCommunication",
-                "rds:RebootDBShardGroup",
-                "rds:ModifyDBProxyEndpoint",
-                "rds:PurchaseReservedDBInstancesOffering",
-                "rds:CreateDBSubnetGroup",
-                "rds:DescribeRecommendations",
-                "rds:ModifyCustomDBEngineVersion",
-                "rds:ModifyDBParameterGroup",
-                "rds:ModifyDBShardGroup",
-                "rds:DownloadDBLogFilePortion",
-                "rds:AddRoleToDBCluster",
-                "rds:DeleteDBInstance",
-                "rds:ModifyDBRecommendation",
-                "rds:DeleteIntegration",
-                "rds:DeleteDBProxy",
-                "rds:DeleteDBInstanceAutomatedBackup",
-                "rds:CreateDBSnapshot",
-                "rds:DeleteDBSecurityGroup",
-                "rds:ModifyRecommendation",
-                "rds:CreateEventSubscription",
-                "rds:ModifyTenantDatabase",
-                "rds:DeleteDBShardGroup",
-                "rds:DeleteOptionGroup",
-                "rds:FailoverDBCluster",
-                "rds:AddRoleToDBInstance",
-                "rds:ModifyDBProxy",
-                "rds:CreateDBInstance",
-                "rds:DescribeRecommendationGroups",
-                "rds:ModifyActivityStream",
-                "rds:DeleteDBCluster",
-                "rds:StartDBInstanceAutomatedBackupsReplication",
-                "rds:ModifyEventSubscription",
-                "rds:ModifyDBProxyTargetGroup",
-                "rds:RebootDBCluster",
-                "rds:ModifyDBSnapshot",
-                "rds:DeleteDBClusterSnapshot",
-                "rds:ListTagsForResource",
-                "rds:CreateDBCluster",
-                "rds:DeleteDBClusterParameterGroup",
-                "rds:ApplyPendingMaintenanceAction",
-                "rds:BacktrackDBCluster",
-                "rds:RemoveRoleFromDBInstance",
-                "rds:ModifyDBSubnetGroup",
-                "rds:FailoverGlobalCluster",
-                "rds:RemoveRoleFromDBCluster",
-                "rds:DeleteTenantDatabase",
-                "rds:CreateGlobalCluster",
-                "rds:DeregisterDBProxyTargets",
-                "rds:CreateOptionGroup",
-                "rds:ModifyIntegration",
-                "rds:CreateDBProxyEndpoint",
-                "rds:AddSourceIdentifierToSubscription",
-                "rds:CopyDBParameterGroup",
-                "rds:CreateDBProxy",
-                "rds:ModifyDBClusterParameterGroup",
-                "rds:ModifyDBInstance",
-                "rds:RegisterDBProxyTargets",
-                "rds:ModifyDBClusterSnapshotAttribute",
-                "rds:CopyDBClusterParameterGroup",
-                "rds:CreateDBClusterEndpoint",
-                "rds:StopDBCluster",
-                "rds:CreateDBParameterGroup",
-                "rds:CancelExportTask",
-                "rds:CreateBlueGreenDeployment",
-                "rds:DeleteDBSnapshot",
-                "rds:RemoveFromGlobalCluster",
-                "rds:DeleteCustomDBEngineVersion",
-                "rds:PromoteReadReplica",
-                "rds:StartDBInstance",
-                "rds:StopActivityStream",
-                "rds:RestoreDBClusterFromS3",
-                "rds:DeleteDBSubnetGroup",
-                "rds:RestoreDBInstanceFromDBSnapshot",
-                "rds:ModifyDBClusterEndpoint",
-                "rds:CreateDBShardGroup",
-                "rds:ModifyDBCluster",
-                "rds:DeleteDBParameterGroup",
-                "rds:CreateDBClusterSnapshot",
-                "rds:CreateDBClusterParameterGroup",
-                "rds:ModifyDBSnapshotAttribute",
-                "rds:DisableHttpEndpoint",
-                "rds:PromoteReadReplicaDBCluster",
-                "rds:ModifyOptionGroup",
-                "rds:RestoreDBClusterFromSnapshot",
-                "rds:StartExportTask",
-                "rds:StartActivityStream",
-                "rds:StopDBInstanceAutomatedBackupsReplication",
-                "rds:DeleteEventSubscription",
-                "rds:RemoveSourceIdentifierFromSubscription",
-                "rds:DeleteDBProxyEndpoint",
-                "rds:DeleteDBClusterEndpoint",
-                "rds:RevokeDBSecurityGroupIngress",
-                "rds:ModifyCurrentDBClusterCapacity",
-                "rds:ResetDBClusterParameterGroup",
-                "rds:RestoreDBClusterToPointInTime",
-                "rds:CreateCustomDBEngineVersion",
-                "rds:CreateIntegration",
-                "rds:CopyDBSnapshot",
-                "rds:CopyDBClusterSnapshot",
-                "rds:SwitchoverBlueGreenDeployment",
-                "rds:StopDBInstance",
-                "rds:CopyOptionGroup",
-                "rds:DeleteDBClusterAutomatedBackup",
-                "rds:SwitchoverReadReplica",
-                "rds:ModifyCertificates",
-                "rds:CreateDBSecurityGroup",
-                "rds:RebootDBInstance",
-                "rds:ModifyGlobalCluster",
-                "rds:EnableHttpEndpoint",
-                "rds:DownloadCompleteDBLogFile",
-                "rds:CreateDBInstanceReadReplica",
-                "rds:SwitchoverGlobalCluster",
-                "rds:CreateTenantDatabase",
-                "rds:RestoreDBInstanceToPointInTime"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
 
-resource "aws_iam_role_policy_attachment" "policy-1-attach" {
-  role       = aws_iam_role.lambda-1-role.name
-  policy_arn = aws_iam_policy.policy-rotation-2.arn
 
-}
 
-resource "aws_iam_role_policy_attachment" "policy-2-attach" {
-  role       = aws_iam_role.lambda-1-role.name
-  policy_arn = aws_iam_policy.policy-2.arn
 
-}
 
-resource "aws_iam_role_policy_attachment" "lambda-2-rotation-policy-1-attach" {
-  role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.policy-rotation-1.arn
-
-}
-
-resource "aws_iam_role_policy_attachment" "lambda-2-rotation-policy-arn-1-attach" {
-  role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-
-}
-
-resource "aws_iam_role_policy_attachment" "lambda-2-rotation-policy-arn-2-attach" {
-  role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-
-}
-
-resource "aws_iam_role_policy_attachment" "lambda-2-rotation-policy-2-attach" {
-  role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.policy-rotation-2.arn
-
-}
-
-# resource "aws_iam_role_policy_attachment" "lambda-2-rotation-policy-3-attach" {
-#   role       = aws_iam_role.lambda-2-role.name
-#   policy_arn = aws_iam_policy.policy-rotation-3.arn
-
-# }
 
--- Project-4/secretsManager.tf
+++ Project-4/secretsManager.tf
@@ -1,17 +1,3 @@
-resource "aws_db_instance" "mysql" {
-  allocated_storage    = 20
-  db_name              = "mydb"
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
-  username             = "admin"
-  password             = aws_secretsmanager_secret_version.sec-version.secret_string
-  parameter_group_name = "default.mysql8.0"
-  skip_final_snapshot  = true
-
-}
-
 resource "random_password" "rand_password" {
   length  = 10
   special = true
@@ -29,8 +15,15 @@ resource "aws_secretsmanager_secret" "rds-secret" {
 resource "aws_secretsmanager_secret_version" "sec-version" {
   secret_id = aws_secretsmanager_secret.rds-secret.id
 
-  secret_string = random_password.rand_password.result
+  secret_string = jsonencode({
+    password = "${random_password.rand_password.result}"
+    username = "admin"
+    dbname   = "mydb"
+
+  })
 }
+
+
 
 
 resource "aws_secretsmanager_secret_rotation" "rotation" {
